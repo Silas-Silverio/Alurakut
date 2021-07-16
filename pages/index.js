48b6ee2acf295726bb30ba66cfd50a2bdd5feccc
Silas Leme Silvério <comikpro22@gmail.com>
2021-07-16 14:45:01 -0300

    inclusão do login

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import nookies from 'nookies'
+import jwt from 'jsonwebtoken'
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
 import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
@@ -42,9 +44,9 @@ function ProfileRelationsBox(propriedades){
   );
 }
 
-export default function Home() {
+export default function Home(props) {
 
-  const githubUser = 'Silas-Silverio';
+  const githubUser = props.githubUser;
   const [comunidades, setComunidades] = React.useState([]);
   const pessoasFavoritas = [
     'andreluizcosta',
@@ -191,4 +193,35 @@ export default function Home() {
       </MainGrid>
     </>
   )
+}
+
+export async function getServerSideProps(context) {
+
+  const cookies = nookies.get(context);
+  const token = cookies.USER_TOKEN;
+  
+
+  const { isAuthenticated } = await fetch('https://alurakut.vercel.app/api/auth', {
+    headers: {
+      Authorization: token
+    }
+  })
+  .then((resposta) => resposta.json())
+  
+  if(!isAuthenticated){
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      }
+    }
+  }
+
+  const { githubUser } = jwt.decode(token);
+
+  return {
+    props: {
+      githubUser
+    },
+  }
 }
